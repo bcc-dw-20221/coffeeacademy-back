aadfb55d22f748ae489e86b128a615630fa3bc02
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-09-25 17:52:36 -0300

    Criando classe disciplina

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from cursos.models import Nota, Curso, Disciplina
 
 # Register your models here.
+admin.site.register(Nota)
+admin.site.register(Curso)
+admin.site.register(Disciplina)
--- cursos/models.py
+++ cursos/models.py
@@ -1,35 +1,89 @@
 from time import clock_settime
 from django.db import models
+"""from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from somewhere import handle_uploaded_file
+from django import forms"""
+
+
 
 # Create your models here.
 class Nota(models.Model):
     # disciplina = ForeignKey
-    
+
     nota_disciplina = models.FloatField(default=0.0, blank=False, null=False)
 
     def __str__(self):
         return f"Nota: Disciplina: {self.nota_disciplina}"
 
+
 class Curso(models.Model):
 
     nome_curso = models.CharField(max_length=150)
 
-    #disciplinas(lista) = ForeignKey
+    # disciplinas(lista) = ForeignKey
 
-    qtd_vagas = models.IntegerField(default=0, null=False, blank=False)
-    
+    quantidade_vagas = models.IntegerField(default=0, null=False, blank=False)
+
     def __str__(self) -> str:
         return f"Curso: {self.nome_curso}"
 
-'''
-class Frequencia():
 
-    Criar objeto modelo
+class Disciplina(models.Model):
+    """id_disciplina = chave primaria"""
 
-    lista = []
-    def add(self, novo):
-        self.lista.append(novo)
-'''
+    nome_disciplina = models.CharField(max_length=150)
 
-    
+    """alunos = chave estrangeira"""
 
+    """curso = chave estrangeira"""
+
+    """professor = chave estrangeira"""
+
+    """semestre = chave estrangeira"""
+
+    quantidade_vagas = models.IntegerField(default=0, null=False, blank=True)
+
+    quantidade_creditos = models.IntegerField(
+        default=0, null=False, blank=True)
+
+    tipo_disciplina_choices = (
+        ('OB', 'Obrigatória'),
+        ('OP', 'Optativa'),
+    )
+
+    # dia_aula = data formato dia
+
+    # hora_aula = data formato hh:mm
+
+    carga_horaria = models.CharField(max_length=150)
+
+    conteudo_ministrado = models.CharField(
+        max_length=255, null=False, blank=True)
+
+    # material_disciplina = upload de arquivos
+
+    def __str__(self) -> str:
+        return f"Disciplina: {self.nome_disciplina}"
+
+
+#Upload de arquivos 
+"""class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    file = forms.FileField()
+
+    def handle_uploaded_file(f):
+        with open('some/file/name.txt', 'wb+') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+
+    def upload_file(request):
+        if request.method == 'POST':
+            form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+                handle_uploaded_file(request.FILES['file'])
+                return HttpResponseRedirect('/success/url/')
+        else:
+            form = UploadFileForm()
+        return render(request, 'upload.html', {'form': form})
+"""
