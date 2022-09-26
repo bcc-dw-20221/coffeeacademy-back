0682e0baaf8d9a716e9e093e4f579daf6d118e24
JonhAlefy <alefyjonh76@gmail.com>
2022-09-25 21:10:19 -0300

    App endereços adicionado

--- endereco/models.py
+++ endereco/models.py
@@ -1,3 +1,13 @@
+from mailbox import NotEmptyError
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Endereco(models.Model):
+    rua = models.CharField(max_length=150, null=False)
+    numero = models.IntegerField()
+    complemento = models.CharField(max_length=150)
+    bairro = models.CharField(max_length=150)
+    cep = models.CharField(max_length=9)
+    cidade = models.CharField(max_length=150)
+
+
